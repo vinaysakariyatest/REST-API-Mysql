30357c166e09c03527b73880738d0054bd7845f9
vinaysakariyatest <sakariyavinay1@gmail.com>
2024-06-14 10:42:58 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require("path");
 const bodyParser = require('body-parser');
+const { Sequelize } = require('sequelize');
 
 require("dotenv").config()
 
